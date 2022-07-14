5a1bfd4339b4367a3c687b64e85494a3926c019f
Raúl Martiarena <raulmartiarena89@gmail.com>
2022-07-14 14:35:11 -0500

    Avance home

---
+++ resources/lightbox/lightbox.js
@@ -0,0 +1,14 @@
+// Function to reveal lightbox and adding YouTube autoplay
+function revealVideo(div,video_id) {
+  var video = document.getElementById(video_id).src;
+  document.getElementById(video_id).src = video+'&autoplay=1'; // adding autoplay to the URL
+  document.getElementById(div).style.display = 'block';
+}
+
+// Hiding the lightbox and removing YouTube autoplay
+function hideVideo(div,video_id) {
+  var video = document.getElementById(video_id).src;
+  var cleaned = video.replace('&autoplay=1',''); // removing autoplay form url
+  document.getElementById(video_id).src = cleaned;
+  document.getElementById(div).style.display = 'none';
+}

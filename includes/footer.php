e3d2fedafffdfe6072fc1b7ba66cd42f609f5341
Raúl Martiarena <raulmartiarena89@gmail.com>
2022-12-27 18:36:24 -0500

    Avance de la web xd

--- includes/footer.php
+++ includes/footer.php
@@ -20,7 +20,7 @@
                 <li>Seesaawiki.jp</li>
                 <li>w.atwiki.jp</li>
             </ul>
-            <h5>Sprite Rippers</h5>
+            <h5>Sprites Ripped by:</h5>
             <ul>
                 <li>Davias</li>
                 <li>Bean</li>
@@ -71,48 +71,63 @@
             <h5>Gr&aacute;ficos</h5>
             <p>De spriters-resource.com & spritedatabase.net:</p>
             <ul>
-                <li>Final Fight 1 & 2</li>
-                <li>Street of Rage 2</li>
-                <li>Pretty Soldier Sailor Moon</li>
-                <li>Karnov's Revenge</li>
-                <li>Double Dragon</li>
-                <li>Savage Reign</li>
-                <li>Fight Fever</li>
-                <li>Guardians</li>
-                <li>Street Fighter 2</li>
-                <li>Ghost Chaser Densei</li>
-                <li>Cadillacs & Dinosaurs</li>
+                <li>Final Fantasy IV, V, VI</li>
+                <li>Romancing Saga 2 & 3</li>
+                <li>DunQuest: Majin Fuuin no Densetsu</li>
+                <li>Chrono Tigger</li>
+                <li>Lufia</li>
+                <li>FEDA: The Emblem of Justice</li>
+                <li>RPG maker 2003 (RTP package)</li>
+                <li>Pretty Soldier Sailor Moon: Another Story * </li>
+                <li>Secret of Evermore</li>
+                <li>Treasure of Rudras</li>
+                <li>Seesaawiki.jp</li>
+                <li>w.atwiki.jp</li>
+            </ul>
+            <h5>Sprite Rippeados por:</h5>
+            <ul>
+                <li>Davias</li>
+                <li>Bean</li>
+                <li>volo</li>
+                <li>Cavery210</li>
+                <li>Xyssia</li>
+                <li>Ryan914</li>
+                <li>WaxPoetic</li>
+                <li>daemoth</li>
+                <li>SmithyGCN</li>
+                <li>WaxPoetic</li>
+                <li>Tonberry2k</li>
+                <li>Hax4Ever</li>
+                <li>Magma MK-II *</li>
+                <li>myaphelion *</li>
+                <li>Rootrick *</li>
             </ul>
             <h5>M&uacute;sica</h5>
             <ul>
-                <li>Ryo ga Gotoku</li>
-                <li>Ryo ga Gotoku 2</li>
-                <li>Ryo ga Gotoku 3</li>
-                <li>Ryo ga Gotoku 4</li>
-                <li>Ryo ga Gotoku 5</li>
-                <li>King of Fighters</li>
-                <li>Crisis Beat</li>
-                <li>Bakuretsu Tenshi</li>
-                <li>The Bouncer</li>
-                <li>Crimson Tears</li>
+                <li>Atelier Iris 1, 2 & 3</li>
+                <li>Mana Khemia 1 & 2</li>
+                <li>Ar tonelico 1 & 2</li>
+                <li>Atelier Escha & Logy</li>
+                <li>Time and Eternity</li>
+                <li>Dragon Star Varnir</li>
+                <li>Mary Skelter</li>
+                <li>Atelier Sophie</li>
+                <li>Atelier Meruru</li>
             </ul>
             <h5>Tema de Cierre:</h5>
             <ul>
-                <li>[BLOODY MOON] (por Gospel of Judas)</li>
+                <li>[The Path You Walk] por Nakagawa Ken (Cantado por: Horie Mami)</li>
             </ul>
             <h5>Efectos de sonido</h5>
             <ul>
-                <li>Final Fight</li>
-                <li>Streets of Rage 2</li>
-                <li>Guardians</li>
-                <li>Denjin Makai</li>
-                <li>Ghost Chaser Densei</li>
-                <li>Crimson Tears</li>
-                <li>Cadillacs & Dinosaurs</li>
+                <li>Final Fantasy Saga</li>
+                <li>RPG maker 2003 (RTP package)</li>
+                <li>Romancing Saga</li>
+                <li>Envato Elements</li>
             </ul>
             <h5>Motor Usado</h5>
             <ul>
-                <li>Beats of Rage (OpenBOR)</li>
+                <li>RPG maker 2003</li>
             </ul>
             <?php } ?>
         </div>

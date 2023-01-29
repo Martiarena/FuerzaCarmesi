15d338d049d2c2152cce9a5ab350b3f7d5f4973e
Raúl Martiarena <raulmartiarena89@gmail.com>
2023-01-29 09:35:04 -0500

    Avance enero

--- includes/fondo-img.php
+++ includes/fondo-img.php
@@ -1,7 +1,9 @@
 <div class="fondo-imagen">
-    <div class="back-stars"></div>
-    <div class="himalia">
+    <div class="himalia-sky">
+        <div class="back-stars"></div>
         <div class="back-sky"></div>
+    </div>
+    <div class="himalia">
         <div class="back-clouds"></div>
         <div class="back-plains"></div>
         <div class="back-forest"></div>
